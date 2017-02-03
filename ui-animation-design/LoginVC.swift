726d1aeb0d65314b0c5ea79b90bc0fdfa6c539c9
ewjc <ericswong7@gmail.com>
2017-02-02 17:48:28 -0800

    added border change for textfield

--- ui-animation-design/LoginVC.swift
+++ ui-animation-design/LoginVC.swift
@@ -12,20 +12,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBottomBorder(textField: textField, borderColor: .black)
     }
     
     @IBOutlet weak var textField: UITextField!
-    
-    func changeTextField() {
-        let border = CALayer()
-        let width = UITextField.layerClass.formatWidth
-        border.backgroundColor = UIColor.black.cgColor
-        border.borderWidth = 2.0
-        border.masksToBounds = true
-        border.m
+
+    func setBottomBorder(textField: UITextField, borderColor: UIColor) {
+        self.textField.borderStyle = .none
+        self.textField.backgroundColor = UIColor.clear
+        let width: CGFloat = 1.0
         
-        
-        
+        let borderFrame = CGRect(x: 0.00, y: self.textField.frame.height - width, width: self.textField.frame.width, height: width)
+        let borderLine = UIView(frame: borderFrame)
+        borderLine.backgroundColor = borderColor
         
     }
     
---
+++ ui-animation-design/TextFieldHelper.swift
@@ -0,0 +1,13 @@
+//
+//  TextFieldHelper.swift
+//  ui-animation-design
+//
+//  Created by Eric Wong on 2/2/17.
+//  Copyright © 2017 Tawpy. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldHelper: UITextField {
+ 
+}
