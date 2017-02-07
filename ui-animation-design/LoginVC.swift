a1e92491f30b40430bcae6abaf47b8c05206d5b0
ewjc <ericswong7@gmail.com>
2017-02-07 10:53:35 -0800

    added frame

--- ui-animation-design/LoginVC.swift
+++ ui-animation-design/LoginVC.swift
@@ -14,9 +14,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         textField.setBottomBorder(borderColor: .black)
         textField.delegate = self
+        createLoginBox()
     }
     
     @IBOutlet weak var textField: UITextField!
+    
+    func createLoginBox() {
+        var originX = self.view.frame.width / 2
+        var originY = self.view.frame.height / 0.5
+        var loginFrame = CGRect(x: 0, y: 0 , width: 150 , height: 50)
+        var loginBoxView = UIView(frame: loginFrame)
+        loginBoxView.backgroundColor = UIColor.clear
+        loginBoxView.layer.borderColor = UIColor.black.cgColor
+        loginBoxView.layer.borderWidth = 3
+        self.view.addSubview(loginBoxView)
+        loginBoxView.c
+    }
 
 }
 
@@ -33,6 +46,8 @@ extension UITextField {
         self.addSubview(borderLine)
         borderLine.backgroundColor = borderColor
     }
+    
+    
     
 }
 
