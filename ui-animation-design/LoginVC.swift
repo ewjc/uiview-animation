81adcd6bdb3d5fa7bdca00416e6f313a62530a20
ewjc <ericswong7@gmail.com>
2017-02-07 11:03:54 -0800

    fixed frame to view

--- ui-animation-design/LoginVC.swift
+++ ui-animation-design/LoginVC.swift
@@ -14,22 +14,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         textField.setBottomBorder(borderColor: .black)
         textField.delegate = self
-        createLoginBox()
-    }
-    
-    @IBOutlet weak var textField: UITextField!
-    
-    func createLoginBox() {
-        var originX = self.view.frame.width / 2
-        var originY = self.view.frame.height / 0.5
-        var loginFrame = CGRect(x: 0, y: 0 , width: 150 , height: 50)
-        var loginBoxView = UIView(frame: loginFrame)
+
+        let originX = self.view.frame.width / 2
+        let originY = self.view.frame.height / 0.5
+        let loginFrame = CGRect(x: 0, y: 0 , width: 150 , height: 50)
+        let loginBoxView = UIView(frame: loginFrame)
         loginBoxView.backgroundColor = UIColor.clear
         loginBoxView.layer.borderColor = UIColor.black.cgColor
         loginBoxView.layer.borderWidth = 3
         self.view.addSubview(loginBoxView)
-        loginBoxView.c
+        loginBoxView.center = CGPoint(x: originX, y: originY)
+    
+        
     }
+    
+    @IBOutlet weak var textField: UITextField!
+    
 
 }
 
